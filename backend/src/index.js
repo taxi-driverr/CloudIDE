1b0c5087747a5ebc42496c711c2d847f9329abf0
taxi_driverr <xerxesxerxes13@gmail.com>
2026-07-28 18:08:27 +0530

    added react query for api fetching

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,13 @@
 import express from "express";
 import cors from "cors";
 import { PORT } from "./config/serverConfig.js";
-
+import apiRouter from "./routes/index.js";
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(cors());
-
+app.use("/api", apiRouter);
 app.get("/ping", (req, res) => {
   return res.json({ message: "pong" });
 });
