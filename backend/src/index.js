0217d45c06d8e18eb1258f062dfb97b92dc92109
taxi_driverr <xerxesxerxes13@gmail.com>
2026-07-28 06:15:44 +0530

    added basic backend setup

---
+++ backend/src/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import { PORT } from "./config/serverConfig.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cors());
+
+app.get("/ping", (req, res) => {
+  return res.json({ message: "pong" });
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running at port : ${PORT}`);
+});
