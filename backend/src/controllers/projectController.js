068c40bc1100973dab273a0dfa72d7b6cc5b7352
taxi_driverr <xerxesxerxes13@gmail.com>
2026-07-29 06:40:33 +0530

    added code for creating a project at backend

---
+++ backend/src/controllers/projectController.js
@@ -0,0 +1,22 @@
+import util from "util";
+import child_process from "child_process";
+import fs from "fs/promises";
+import uuid4 from "uuid4";
+const execPromisified = util.promisify(child_process.exec);
+export const createProjectController = async (req, res) => {
+  const projectId = uuid4();
+  console.log("New project id is", projectId);
+
+  await fs.mkdir(`./projects/${projectId}`);
+
+  // After this call the npm creaste vite command in the newly created project folder
+
+  const response = await execPromisified(
+    "npm create vite@latest sandbox -- --template react",
+    {
+      cwd: `./projects/${projectId}`,
+    },
+  );
+
+  return res.json({ message: "Project created", data: projectId });
+};
